034db4d5268fd2df128a221d925b06a1824242c4
Gaurav Singh <sgaurav4683@gmail.com>
2023-05-14 21:18:19 +0530

    toggle mode added

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -25,7 +25,10 @@ export default function TextForm(props) {
 
   return (
     <>
-      <div className="container">
+      <div
+        className="container"
+        style={{ color: props.mode === "dark" ? "white" : "black" }}
+      >
         <h1>{props.heading}</h1>
         <div className="mb-3">
           <textarea
@@ -34,6 +37,10 @@ export default function TextForm(props) {
             value={text}
             id="myBox"
             rows="8"
+            style={{
+              backgroundColor: props.mode === "dark" ? "grey" : "white",
+              color: props.mode === "dark" ? "white" : "black",
+            }}
           ></textarea>
         </div>
         <button className="btn btn-primary mx-2" onClick={handleUpClick}>
@@ -46,14 +53,17 @@ export default function TextForm(props) {
           Clear Text
         </button>
       </div>
-      <div className="container">
+      <div
+        className="container"
+        style={{ color: props.mode === "dark" ? "white" : "black" }}
+      >
         <h2>Input Text Summary</h2>
         <p>
           {text.split(" ").length} words and {text.length} characters
         </p>
         <p>{0.008 * text.split("").length} Minutes read</p>
         <h2>Preview</h2>
-        <p>{text}</p>
+        <p>{text.length > 0 ? text : "Nothing to preview."}</p>
       </div>
     </>
   );
